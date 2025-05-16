250e8690852e5a65c83f30975498065f422035a0
Dalvy07 <vladlulka3@gmail.com>
2025-05-16 04:10:55 +0200

    + Email verification BUT ALL CODE REALLY NEED REFACTORIZATION and some standartization

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -19,21 +19,40 @@ const authController = {
 
     activateEmail: asyncHandler(async (req, res) => {
         const activationLink = req.params.link;
-        await authService.activateEmail(activationLink);
 
-        res.redirect(process.env.CLIENT_URL);
+        console.log('URL params:', req.params);
+        console.log('Extracted link:', activationLink);
+
+        const result = await authService.activateEmail(activationLink);
+
+        // res.redirect(process.env.CLIENT_URL);
+        res.status(200).json(ApiResponse.success(result.user, 'Email verification successful'));
+    }),
+
+    resendVerificationEmail: asyncHandler(async (req, res) => {
+        const userId = req.user.id;
+        const result = await authService.resendVerificationEmail(userId);
+        
+        // Ошибки выбрасываются в сервисе
+        res.status(200).json(ApiResponse.success(null, result.message));
     }),
 
     loginUser: asyncHandler(async (req, res) => {
         const { email, password } = req.body;
-        const user = await authService.loginUser(email, password);
+        const userInfo = await authService.loginUser(email, password);
 
-        res.cookie('refreshToken', user.refreshToken, {
+        res.cookie('refreshToken', userInfo.refreshToken, {
             maxAge: parseInt(process.env.JWT_REFRESH_EXPIRE_SEC) * 1000,
             httpOnly: true
             // secure: process.env.NODE_ENV === 'production',
         });
-        res.status(200).json(ApiResponse.success(user, 'User logged in successfully'));
+
+        var message = 'User logged in successfully';
+        if (!userInfo.user.isEmailVerified) {
+            message += '. Please verify your email to access all features';
+        }
+        
+        res.status(200).json(ApiResponse.success(userInfo, message));
     }),
 
     logoutUser: asyncHandler(async (req, res) => {
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const authenticate = require('../middleware/auth.middleware');
+const checkRole = require('../middleware/checkRole.middleware');
+
 
 const authController = require('../controllers/auth.controller');
 
@@ -12,13 +15,13 @@ const {
 } = require('../validators/auth.validator');
 const validate = require('../middleware/validate.middleware');
 
-// Публичные маршруты
+// Публичные маршруты после регестрации на сервере будут доступны по /auth
 router.post('/register', registerMinimalSchema, validate, authController.registerUserMinimal);
 router.post('/login', loginSchema, validate, authController.loginUser);
 router.post('/logout', logoutSchema, validate, authController.logoutUser);
 router.get('/refresh', refreshTokenSchema, validate, authController.refreshToken);
 
-router.get('/activate/:link', activateEmailSchema, validate, authController.activateEmail);
-
+router.get('/activate/:link', authController.activateEmail);
+router.post('/resend-verification', authenticate, authController.resendVerificationEmail);
 
 module.exports = router;
